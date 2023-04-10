da5d4036545008966bfd9b0ec2da9c0c611791d2
Oleks Hetman <oleksiygord@gmail.com>
2023-04-10 12:54:46 +0300

    eCommerce

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://vexavv:qlOxzGSShJT34c4x@fe5.ypvfdjg.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',
+};
